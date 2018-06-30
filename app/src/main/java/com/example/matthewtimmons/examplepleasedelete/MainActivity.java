5a755a188e3230188e3ed1dd19dc966ccc2d79e1
matthewtimmons <Matthwebt@yahoo.com>
2018-06-30 16:01:16 -0500

    Adds Bottom Navigation View to app. Bottom Navigation View contains three not-yet-functioning buttons: Concerts, Games, and Movies.

--- app/src/main/java/com/example/matthewtimmons/examplepleasedelete/MainActivity.java
+++ app/src/main/java/com/example/matthewtimmons/examplepleasedelete/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.matthewtimmons.examplepleasedelete;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.pager);
         FragmentManager fragmentManager = getSupportFragmentManager();
         viewPager.setAdapter(new Adapter(fragmentManager));
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.action_concerts:
+                            Toast.makeText(MainActivity.this, "Concerts clicked", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.action_games:
+                            Toast.makeText(MainActivity.this, "Games clicked", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.action_movies:
+                            Toast.makeText(MainActivity.this, "Movies clicked", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                    return true;
+            }
+        });
     }
 }
