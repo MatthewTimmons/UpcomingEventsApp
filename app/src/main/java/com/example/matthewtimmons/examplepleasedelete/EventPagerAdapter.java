effc4107a11635b3316d2b269374d91c412db26b
matthewtimmons <Matthwebt@yahoo.com>
2018-06-30 18:21:59 -0500

    Adds nav bar button functionality to change pages when clicked.

---
+++ app/src/main/java/com/example/matthewtimmons/examplepleasedelete/EventPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.matthewtimmons.examplepleasedelete;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class EventPagerAdapter extends FragmentPagerAdapter {
+
+    private static final int COUNT_PAGES = 3;
+    public static final int INDEX_CONCERTS = 0;
+    public static final int INDEX_GAMES = 1;
+    public static final int INDEX_MOVIES = 2;
+
+    public EventPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = null;
+        switch (i) {
+            case INDEX_CONCERTS:
+                fragment = new ConcertsFragment();
+                break;
+            case INDEX_GAMES:
+                fragment = new GamesFragment();
+                break;
+            case INDEX_MOVIES:
+                fragment = new MoviesFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return COUNT_PAGES;
+    }
+}
--- app/src/main/java/com/example/matthewtimmons/examplepleasedelete/MainActivity.java
+++ app/src/main/java/com/example/matthewtimmons/examplepleasedelete/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.matthewtimmons.examplepleasedelete;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,29 +14,32 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ViewPager viewPager = null;
+    ViewPager viewPager;
+    FragmentPagerAdapter pagerAdapter;
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         viewPager = findViewById(R.id.pager);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        viewPager.setAdapter(new Adapter(fragmentManager));
+        pagerAdapter = new EventPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                     switch (menuItem.getItemId()) {
                         case R.id.action_concerts:
-                            Toast.makeText(MainActivity.this, "Concerts clicked", Toast.LENGTH_SHORT).show();
+                            viewPager.setCurrentItem(0);
                             break;
                         case R.id.action_games:
-                            Toast.makeText(MainActivity.this, "Games clicked", Toast.LENGTH_SHORT).show();
+                            viewPager.setCurrentItem(1);
                             break;
                         case R.id.action_movies:
-                            Toast.makeText(MainActivity.this, "Movies clicked", Toast.LENGTH_SHORT).show();
+                            viewPager.setCurrentItem(2);
                             break;
                     }
                     return true;
