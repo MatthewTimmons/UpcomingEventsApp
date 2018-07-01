fda6f9f92b43f808883ba346cbadb5f949cce414
matthewtimmons <Matthwebt@yahoo.com>
2018-07-01 10:11:28 -0500

    Fixes recycler view in Concerts tab, so the pager gets updated while permitting the navigation bar and layout to remain.

--- app/src/main/java/com/matthewtimmons/upcomingeventsapp/ConcertsFragment.java
+++ app/src/main/java/com/matthewtimmons/upcomingeventsapp/ConcertsFragment.java
@@ -1,16 +1,37 @@
 package com.matthewtimmons.upcomingeventsapp;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.matthewtimmons.upcomingeventsapp.R;
+import com.matthewtimmons.upcomingeventsapp.models.Concert;
+import com.matthewtimmons.upcomingeventsapp.views.ConcertListAdapter;
 
 public class ConcertsFragment extends Fragment {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    ConcertListAdapter concertAdapter;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_concerts, container, false);
+        View v = inflater.inflate(R.layout.fragment_concerts, container, false);
+
+        recyclerView = v.findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        concertAdapter = new ConcertListAdapter(Concert.getPlaceholderConcerts());
+        recyclerView.setAdapter(concertAdapter);
+
+        return v;
     }
+
+
 }
--- app/src/main/java/com/matthewtimmons/upcomingeventsapp/MainActivity.java
+++ app/src/main/java/com/matthewtimmons/upcomingeventsapp/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 
 import com.matthewtimmons.upcomingeventsapp.R;
@@ -15,14 +13,9 @@ import com.matthewtimmons.upcomingeventsapp.models.Concert;
 import com.matthewtimmons.upcomingeventsapp.views.ConcertListAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
     ViewPager viewPager;
     FragmentPagerAdapter pagerAdapter;
     BottomNavigationView bottomNavigationView;
-    RecyclerView recyclerView;
-    RecyclerView.Adapter adapter;
-    RecyclerView.LayoutManager layoutManager;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,33 +27,22 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(pagerAdapter);
 
 
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                     switch (menuItem.getItemId()) {
                         case R.id.action_concerts:
-                            viewPager.setCurrentItem(0);
-                            loadConcertsFragment();
+                            viewPager.setCurrentItem(EventPagerAdapter.INDEX_CONCERTS);
                             break;
                         case R.id.action_games:
-                            viewPager.setCurrentItem(1);
+                            viewPager.setCurrentItem(EventPagerAdapter.INDEX_GAMES);
                             break;
                         case R.id.action_movies:
-                            viewPager.setCurrentItem(2);
+                            viewPager.setCurrentItem(EventPagerAdapter.INDEX_MOVIES);
                             break;
                     }
                     return true;
             }
         });
-    }
-
-    private void loadConcertsFragment() {
-        setContentView(R.layout.fragment_concerts);
-        ConcertListAdapter concertAdapter = new ConcertListAdapter(Concert.getPlaceholderConcerts());
-        recyclerView = findViewById(R.id.recycler_view);
-        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(concertAdapter);
     }
 }
